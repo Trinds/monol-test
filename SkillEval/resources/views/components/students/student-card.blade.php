84d337f1da6ec8f7637bf0b08942ec2d8f52b509
Trinds <carlos.bem.t0123933@edu.atec.pt>
2023-10-04 00:50:49 +0100

    fixed student card(not finished)

--- SkillEval/resources/views/components/students/student-card.blade.php
+++ SkillEval/resources/views/components/students/student-card.blade.php
@@ -1,8 +1,8 @@
 <div class="student-card">
     <h1>{{$student->name}}</h1>
     <img class="user-img" src="{{$student->image}}" alt="User's Profile Picture">
-    <label>Email:</label><input type="text" value="{{$student->email}}" disabled><br>
-    <label>Turma:</label><input type="text" value="{{--{{$student->classroom->edition}}--}}" disabled><br>
-    <label>Nº Aluno:</label><input type="text" value="{{$student->student_number}}" disabled><br>
-    <label>Data de nascimento</label><input type="text" value="{{$student->birth_date}}" disabled><br>
+    <label>Email:<input type="text" value="{{$student->email}}" disabled></label>
+    <label>Turma:<input type="text" value="{{--{{$student->classroom->edition}}--}}" disabled></label>
+    <label>Nº Aluno:<input type="text" value="{{$student->student_number}}" disabled></label>
+    <label>Data de nascimento:<input type="text" value="{{$student->birth_date}}" disabled></label>
 </div>
