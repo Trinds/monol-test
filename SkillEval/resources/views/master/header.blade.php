89666eb06b27ad38a2cfa513d7d0c88ee3356a87
Trinds <carlos.bem.t0123933@edu.atec.pt>
2023-10-03 03:42:53 +0100

    added master page

---
+++ SkillEval/resources/views/master/header.blade.php
@@ -0,0 +1,28 @@
+<aside class="sidebar">
+
+    <div class="app-title">
+        <svg width="6" height="39" viewBox="0 0 6 39" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3 0L3 39" stroke="#F8D442" stroke-width="6"></path>
+        </svg>
+        <p>ATEC SkillEval</p>
+    </div>
+    <ul id="userInfo">
+        <li><div><img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=1974&amp;q=80" alt=""></div>
+        </li>
+        <li id="userName">Silvia Milhazes</li>
+        <li id="userRole">Secretária</li>
+    </ul>
+
+    <ul id="menu">
+        <li id="toHome"><div class="icon"><i class="fa fa-home"></i></div>Início</li>
+        <li id="toCourses"><div class="icon"><i class="fa-regular fa-bookmark"></i></div>Cursos</li>
+        <li id="toStudents"><div class="icon"><i class="fa-solid fa-user-graduate"></i></div>Formandos</li>
+        <li id="toClassrooms"><div class="icon"><i class="fa-solid fa-users"></i></div>Turmas</li>
+        <li id="toReports"><div class="icon"><i class="fa fa-flag"></i></div>Relatórios</li>
+        <li id="toEvaluations"><div class="icon"><i class="fa-solid fa-check"></i></div>Avaliações</li>
+    </ul>
+
+    <ul id="sair">
+        <li id="toLogout">SAIR<div class="icon"><i class="fa-solid fa-arrow-right-from-bracket"></i></div></li>
+    </ul>
+</aside>
