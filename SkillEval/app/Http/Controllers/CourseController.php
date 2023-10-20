b3ec3ab651509f4c378cc44d310518e0afc39cd1
Trinds <carlos.bem.t0123933@edu.atec.pt>
2023-10-20 22:25:20 +0100

    added search for users, and removed scrollbar in side-menu (still scrollable)

--- SkillEval/app/Http/Controllers/CourseController.php
+++ SkillEval/app/Http/Controllers/CourseController.php
@@ -29,7 +29,7 @@ class CourseController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
      */
     public function create()
     {
--- SkillEval/app/Http/Controllers/UserController.php
+++ SkillEval/app/Http/Controllers/UserController.php
@@ -10,11 +10,22 @@ use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    public function index()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function index(Request $request)
     {
-        $users = User::with('roles')->get();
+        isset($request->searchParam) ?
+            $users = User::query()
+                ->where(strtoupper('name'), 'LIKE', '%' . strtoupper($request->searchParam) . '%')
+                ->orWhere(strtoupper('email'), 'LIKE', '%' . strtoupper($request->searchParam) . '%')
+                ->paginate(8)->withQueryString()
+            :
+            $users = User::with('roles')->paginate(8)->withQueryString();
 
-        return view('users.index', compact('users'));
+        return view('users.index', ['users' => $users]);
     }
 
     public function edit(User $user)
@@ -34,7 +45,7 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-     public function create() 
+     public function create()
      {
             $roles = Role::all();
             return view('users.create', compact('roles'));
@@ -43,7 +54,7 @@ class UserController extends Controller
      {
         $imagePath = null;
 
-        
+
          $customMessages = [
              'required' => 'O :attribute é obrigatório.',
              'email' => 'O :attribute deve ser um endereço de email válido.',
@@ -51,7 +62,7 @@ class UserController extends Controller
              'password.min' => 'A password deve ter pelo menos :min caracteres.',
              'image' => 'A imagem deve ser um ficheiro do tipo jpeg, png, jpg, gif ou svg com um tamanho máximo de 5 megabytes.'
          ];
-     
+
          $validator = Validator::make($request->all(), [
              'name' => 'required|string|max:255',
              'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:5120',
@@ -59,35 +70,35 @@ class UserController extends Controller
              'password' => 'required|confirmed|min:6',
              'roles' => 'required',
          ], $customMessages);
-     
+
          if ($validator->fails()) {
              return redirect()->back()
                  ->withErrors($validator)
                  ->withInput()
                  ->with('error', 'Oops! Algo correu mal ao criar o utilizador. Por favor verifique o(s) seguinte(s) erro:');
          }
-     
+
          try {
              $user = new User();
              $user->name = $request->input('name');
              $user->email = $request->input('email');
-             
+
                 if ($request->hasFile('image')) {
                     $imagePath = $request->file('image')->store('public/images');
                     $imagePath = str_replace('public/', '', $imagePath);
                     $user->image = $imagePath;
                 }
-             
+
              $user->password = bcrypt($request->input('password'));
              $user->save();
              $user->roles()->sync($request->input('roles'));
-     
+
              return redirect()->route('users.index')->with('success', 'Utilizador criado com sucesso.');
          } catch (Exception $e) {
              return redirect()->back()->with('error', 'Ocorreu um erro ao criar o utilizador.');
          }
      }
-     
+
 
 
     public function update(Request $request, User $user)
@@ -133,16 +144,16 @@ class UserController extends Controller
             if ($request->filled('password')) {
                 if (strlen($request->input('password')) < 6) {
                     $validator->getMessageBag()->add('password', 'A password deve ter pelo menos 6 caracteres.');
-    
+
                     return redirect()->back()
                         ->withErrors($validator)
                         ->withInput()
                         ->with('error', 'Oops! Algo correu mal ao atualizar o utilizador. Por favor verifique o(s) seguinte(s) erro:');
                 }
-    
+
                 $user->password = bcrypt($request->input('password'));
             }
-    
+
             $user->roles()->sync($request->input('roles'));
             $user->save();
 
