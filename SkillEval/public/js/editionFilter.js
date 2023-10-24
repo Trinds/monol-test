87d9b38fb227222c77f8fbfaf7d32c9ffc34a185
JoseCMRibeiro <119082696+JoseCMRibeiro@users.noreply.github.com>
2023-10-24 23:37:22 +0100

    evaluation

--- SkillEval/public/js/editionFilter.js
+++ SkillEval/public/js/editionFilter.js
@@ -1,39 +1,37 @@
-document.addEventListener("DOMContentLoaded", function () 
-{
-    const course_id = document.getElementById("course_id");
+document.addEventListener("DOMContentLoaded", function () {
+    const courseSelect = document.getElementById("course_id");
     const classroomSelect = document.getElementById("classroom_id");
+   
 
-    // function clearClassrooms() 
-    // {
-    //     for (const option of classroomSelect.options) 
-    //         option.style.display = "none";
-    // }
+    function clearClassrooms() {
+        for (const option of classroomSelect.options) {
+            option.style.display = "none";
+        }
+    }
 
-    //clearClassrooms();
+    clearClassrooms();
 
-    function updateClassrooms() 
-    {
-        const selectedCourseId = course_id.value;
-
-        if (selectedCourseId === "") {}
-            //clearClassrooms();
-        else 
-        {
-            for (const option of classroomSelect.options) 
-            {
-                if (option.dataset.course === selectedCourseId) 
+    function updateClassrooms() {
+        const selectedCourseId = courseSelect.value;
+        if (selectedCourseId === "") {
+            clearClassrooms();
+        } else {
+            for (const option of classroomSelect.options) {
+                if (option.getAttribute('data-course') === selectedCourseId) {
                     option.style.display = "block";
-                else
-                    option.style.display = "none";                
+                } else {
+                    option.style.display = "none";
+                }
             }
         }
     }
 
-    course_id.addEventListener("change", function () 
-    {
-        //updateClassrooms();
-        //classroomSelect.value = "";
+    courseSelect.addEventListener("change", function () {
+        updateClassrooms();
+        classroomSelect.value = "";
     });
 
-    //updateClassrooms();
+
+
+    updateClassrooms();
 });
--- SkillEval/routes/web.php
+++ SkillEval/routes/web.php
@@ -29,6 +29,8 @@ Route::resource('reports', 'ReportsController');
 Route::resource('students', 'StudentController');
 Route::resource('classrooms', 'ClassroomController');
 Route::resource('evaluations', 'EvaluationController');
+Route::post('/evaluations', 'EvaluationController@store')->name('evaluations.store');
+
 
 
 
