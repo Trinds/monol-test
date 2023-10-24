e2da74444956fcedd53e08804f30a106c0b1f214
JoseCMRibeiro <119082696+JoseCMRibeiro@users.noreply.github.com>
2023-10-24 16:04:51 +0100

    js

--- SkillEval/public/js/classroomsFilter.js
+++ SkillEval/public/js/classroomsFilter.js
@@ -1,39 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () 
-{
+document.addEventListener("DOMContentLoaded", function () {
     const course_id = document.getElementById("course_id");
     const classroomSelect = document.getElementById("classroom_id");
 
-    // function clearClassrooms() 
-    // {
-    //     for (const option of classroomSelect.options) 
-    //         option.style.display = "none";
-    // }
-
-    //clearClassrooms();
-
-    function updateClassrooms() 
-    {
+    function updateClassrooms() {
         const selectedCourseId = course_id.value;
-
-        if (selectedCourseId === "") 
-            clearClassrooms();
-        else 
-        {
-            for (const option of classroomSelect.options) 
-            {
-                if (option.dataset.course === selectedCourseId) 
-                    option.style.display = "block";
-                else
-                    option.style.display = "none";                
+        
+        for (const option of classroomSelect.options) {
+            selectedCourseId === "" ||
+            option.dataset.course === selectedCourseId
+                ? (option.style.display = "block")
+                : 
+                ((option.style.display = "none"));
             }
         }
-    }
-
-    course_id.addEventListener("change", function () 
-    {
+        
+        course_id.addEventListener("change", function () {
+            updateClassrooms();
+                classroomSelect.value = "";
+        });
         updateClassrooms();
-        classroomSelect.value = "";
     });
-
-    updateClassrooms();
-});
